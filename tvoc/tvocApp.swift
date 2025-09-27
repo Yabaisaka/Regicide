e4e91dc383cb638f7438860d1e8d26bb5cfc7c2f
Cocona <lyk20010305@gmail.com>
2025-09-27 17:00:27 +0800

    Initial Commit

---
+++ tvoc/tvocApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvocApp.swift
+//  tvoc
+//
+//  Created by C-ocona on 2025/9/27.
+//
+
+import SwiftUI
+
+@main
+struct tvocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tvocTests/tvocTests.swift
@@ -0,0 +1,17 @@
+//
+//  tvocTests.swift
+//  tvocTests
+//
+//  Created by C-ocona on 2025/9/27.
+//
+
+import Testing
+@testable import tvoc
+
+struct tvocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
